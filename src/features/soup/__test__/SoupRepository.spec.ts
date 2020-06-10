c3b232a297ca628de6ee05d7ad4c4f720d99da04
Charles <charles26.dev@gmail.com>
2020-06-10 04:28:49 -0300

    test de repository con snapshot

--- src/features/soup/__test__/SoupRepository.spec.ts
+++ src/features/soup/__test__/SoupRepository.spec.ts
@@ -5,7 +5,6 @@ describe('SoupRepository', () => {
   it('CreateSummary', () => {
     const words: string[] = ["oiE", "ixE", "Ix", "i", "O", "awds", "e4", "", "oi", "eX"];
     let summary: Summary = SoupRepository.CreateSummary(words);
-
     expect(summary).toMatchSnapshot();
   });
 
@@ -24,7 +23,53 @@ describe('SoupRepository', () => {
     soup.forEach((row: string[]) => {
       row.forEach((letter: string) => {
         letter = letter.toUpperCase();
-        summary = SoupRepository.LookUpOneLetterWords(letter, summary);
+        SoupRepository.LookUpOneLetterWords(letter, summary);
+      });
+    });
+
+    expect(summary).toMatchSnapshot();
+  });
+
+  it('LookToTheRight', () => {
+    const words: string[] = ["oiE", "Oi", "ie", "ii", "eXe", "EX", "XE", "X4", "IIX4", "", "palabraSuperGrande"];
+    let summary: Summary = SoupRepository.CreateSummary(words);
+    const soup: string[][] = [
+      ["o", "I", "E"],
+      ["I", "i", "X", "4"],
+      ["E", "x", "E"],
+      ["E", "X"],
+      ["e"],
+      [""]
+    ];
+
+    soup.forEach((row: string[], rowIndex: number) => {
+      const numberColumns: number = row.length - 1;
+      row.forEach((letter: string, columnIndex: number) => {
+        letter = letter.toUpperCase();
+        SoupRepository.LookToTheRight(columnIndex, numberColumns, summary, row);
+      });
+    });
+
+    expect(summary).toMatchSnapshot();
+  });
+
+  it('LookAtDiagonal', () => {
+    const words: string[] = ["palabraSuperGrande", "oie", "OI", "iX", "E4", "Ex", ""];
+    let summary: Summary = SoupRepository.CreateSummary(words);
+    const soup: string[][] = [
+      ["o", "I", "E"],
+      ["I", "i", "X", "4"],
+      ["E", "x", "E"],
+      ["E", "X"],
+      ["e"],
+      [""]
+    ];
+
+    soup.forEach((row: string[], rowIndex: number) => {
+      const numberColumns: number = row.length - 1;
+      row.forEach((letter: string, columnIndex: number) => {
+        letter = letter.toUpperCase();
+        SoupRepository.LookAtDiagonal(rowIndex, columnIndex, summary, soup);
       });
     });
 
